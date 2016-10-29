1a7d85c13a3f67649a0d40a0dee4d5487a23a0c9
Luis Felipe Henao López <lfhenaolopez@misena.edu.co>
2016-10-29 03:09:36 -0500

    Commit

--- assets/js/app/app.config.js
+++ assets/js/app/app.config.js
@@ -14,6 +14,54 @@
             .state('objetivos', {
                 url: "/objetivos",
                 templateUrl: "objetivos.html"
+            })
+            .state('justificacion', {
+                url: "/justificacion",
+                templateUrl: "justificacion.html"
+            })
+            .state('antecedentes', {
+                url: "/antecedentes",
+                templateUrl: "antecedentes.html"
+            })
+            .state('legal', {
+                url: "/legal",
+                templateUrl: "estudio_legal.html"
+            })
+            .state('ambiental', {
+                url: "/ambiental",
+                templateUrl: "estudio_ambiental.html"
+            })
+            .state('organizacional', {
+                url: "/organizacional",
+                templateUrl: "estudio_organizacional.html"
+            })
+            .state('entorno', {
+                url: "/entorno",
+                templateUrl: "estudio_entorno.html"
+            })
+            .state('tecnico', {
+                url: "/tecnico",
+                templateUrl: "estudio_tecnico.html"
+            })
+            .state('mercado', {
+                url: "/mercado",
+                templateUrl: "estudio_mercado.html"
+            })
+            .state('financiacion', {
+                url: "/financiacion",
+                templateUrl: "fuentes_financiacion.html"
+            })
+            .state('financiera', {
+                url: "/financiera",
+                templateUrl: "evaluacion_financiera.html"
+            })
+            .state('integrantes', {
+                url: "/integrantes",
+                templateUrl: "about.html"
+            })
+            .state('contact', {
+                url: "/contact",
+                templateUrl: "contact.html"
             });
 
 
